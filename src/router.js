f1d881e060092b95bae4547210707f48e23b53d8
ycc1998 <1391958199@qq.com>
2019-09-26 13:31:56 +0800

    1

--- src/router.js
+++ src/router.js
@@ -1,13 +1,19 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Recommend from './components/recommend/recommend';
-import Singer from './components/singer/singer';
-import Rank from './components/rank/rank';
-import Search from './components/search/search';
+//import Recommend from './components/recommend/recommend';
+//import Singer from './components/singer/singer';
+//import Rank from './components/rank/rank';
+//import Search from './components/search/search';
 
 import SingerDetail from './components/singer-detail/singer-detail';
 import Disc from './components/disc/disc';
 import TopList from './components/top-list/top-list';
+
+//路由懒加载
+const Recommend = () => import('./components/recommend/recommend')
+const Singer = () => import('./components/singer/singer')
+const Rank = () => import('./components/rank/rank')
+const Search = () => import('./components/search/search')
 
 Vue.use(Router);
 
